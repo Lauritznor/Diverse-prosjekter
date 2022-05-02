85354445e740d2adfa7780981645ec7f86a4f534
lauritznor <lauritz-1999@live.com>
2022-05-02 12:26:10 +0200

    test vue bootstrap

---
+++ src/components/pages/DataInserts/Tab.vue
@@ -0,0 +1,19 @@
+<template>
+<div v-if="isSelected">
+</div> 
+
+    
+</template>
+<script>
+export default {
+    props: {
+        isSelected: {
+            type: Boolean
+        }
+    }
+    
+}
+</script>
+<style scoped>
+
+</style>
