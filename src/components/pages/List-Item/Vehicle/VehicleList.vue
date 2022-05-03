9b19bee51faed6ea0c2fc77a32c01f8825035166
lauritznor <lauritz-1999@live.com>
2022-05-03 15:20:20 +0200

    123

---
+++ src/components/pages/List-Item/Vehicle/VehicleList.vue
@@ -0,0 +1,32 @@
+<template>
+    <section>
+        <vehicle-item v-for="(vehicle, i) in vehicles"
+        :key="i"
+            :id="vehicle.id"
+            :vehicleName="vehicle.vehicleName"
+            :vehicleType="vehicle.vehicleType"
+            :weight="vehicle.weight"
+            :isArmored="vehicle.isArmored"
+            
+        ></vehicle-item>
+    </section>
+</template>
+
+<script>
+import VehicleItem from './VehicleItem.vue'
+import vehicleService from '../../../../services/vehicleService.js'
+
+export default {
+    setup(){
+        const vehicles = vehicleService.getAllVehicles();
+
+        return { 
+            vehicles 
+        }
+    },
+    components: {
+        VehicleItem
+    }
+}
+
+</script>
